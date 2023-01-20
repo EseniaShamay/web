970dbccd25a964897f625e395a570df2e1e9db38
buharinovaMasha <121102291+buharinovaMasha@users.noreply.github.com>
2023-01-20 22:37:47 +0500

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,73 @@
+<?php 
+  include "path.php";
+  include "app/controllers/users.php";
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- Custom Styling -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Flower Fairy</title>
+  </head>
+
+  
+  <body>
+    <?php include("app/include/header.php"); ?>
+
+  <!-- END HEADER -->
+<!-- FORM -->
+<div class="container reg_form">
+  <form class="row justify-content-center" method="post" action="reg.php">
+    <h2>Форма регистрации</h2>
+    <div class="mb-3 col-12 col-md-4 err">
+      <p><?=$errMsg?></p>
+    </div>
+    <div class="w-100"></div>
+    <div class="mb-3 col-12 col-md-4">
+      <label for="formGroupExampleInput" class="form-label">Логин</label>
+      <input name="login" value="<?=$login?>" type="text" class="form-control" id="formGroupExampleInput">
+    </div>
+    <div class="w-100"></div>
+    <div class="mb-3 col-12 col-md-4">
+      <label for="exampleInputEmail1" class="form-label">Email</label>
+      <input name="mail" value="<?=$email?>" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <div id="emailHelp" class="form-text">Мы не будем использовать Вашу почту для спама.</div>
+    </div>
+    <div class="w-100"></div>
+    <div class="mb-3 col-12 col-md-4">
+      <label for="exampleInputPassword1" class="form-label">Пароль</label>
+      <input name="pass-first" type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="w-100"></div>
+    <div class="mb-3 col-12 col-md-4">
+      <label for="exampleInputPassword2" class="form-label">Повторите пароль</label>
+      <input type="password" class="form-control" id="exampleInputPassword2">
+    </div>
+    <div class="w-100"></div>
+    <div class="mb-3 col-12 col-md-4">
+      <button name="button-reg" type="submit" class="btn btn-secondary">Регистрация</button>
+      <a href="aut.html">Войти</a>
+    </div>
+    
+  </form>
+</div>
+
+<!-- блок footer-->
+<?php include("app/include/footer.php") ?>;
+
+  </body>
+</html>
