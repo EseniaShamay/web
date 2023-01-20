474b60fbb8b25eeb7d4bd57919c82c0ba5ecf3eb
EsyaShamay <93381874+EseniaShamay@users.noreply.github.com>
2023-01-20 22:37:45 +0500

    app

---
+++ app/controllers/users.php
@@ -0,0 +1,72 @@
+<?php
+include "app/database/db.php";
+
+$errMsg='';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['button-reg'])){
+    $login = trim($_POST['login']);
+    $email = trim($_POST['mail']);
+    $passF=trim($_POST['pass-first']);
+    $passS=trim($_POST['pass-second']);
+    $admin = 0;
+
+    if($login === '' || $email === '' || $passF === '') {
+        $errMsg="Не все поля заполнены!";
+    }elseif (mb_strlen($login, 'UTF8') < 2) {
+        $errMsg = "Логин должен быть более 2-ч символов";
+    }elseif ($passF === $passS) {
+        $errMsg = "Пароли должны совпадать в обоих полях!";
+    }else{
+        $existence = selectOne('users', ['email' => $email]);
+        if  (!empty($existence['email']) && $existence['email'] === $email){
+            $errMsg = "Пользователь с такой почтой уже зарегистрирован!";
+        }else{
+            $pass = password_hash($passF, PASSWORD_DEFAULT);
+            $post = [
+                'admin' => $admin, 
+                'username' => $login, 
+                'email' => $email, 
+                'password' => $pass
+            ];
+            $id = insert('users', $post);
+            $user = selectOne('users', ['id' => $id]);
+
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['login'] = $user['username'];
+            $_SESSION['admin'] = $user['admin'];
+
+            if($_SESSION['admin']){
+                header('location: ' . BASE_URL . admin/admin.php);
+            }else{
+                header('location: ' . BASE_URL);
+            }
+            
+        }
+        
+    }
+}else{
+    $login = '';
+    $email = '';
+}
+
+// Код для формы авторизации
+if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['button-log'])){
+
+    $email = trim($_POST['mail']);
+    $pass = trim($_POST['password']);
+
+    if($email === '' || $pass === '') {
+        array_push($errMsg, "Не все поля заполнены!");
+    }else{
+        $existence = selectOne('users', ['email' => $email]);
+        if($existence && password_verify($pass, $existence['password'])){
+            userAuth($existence);
+        }else{
+            array_push($errMsg, "Почта либо пароль введены неверно!");
+        }
+    }
+}else{
+    $email = '';
+}
+
+?>
---
+++ app/include/header.php
@@ -0,0 +1,39 @@
+<header class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h1>
+                    <a href="<?php echo BASE_URL ?>">Flower Fairy</a>
+                </h1>
+            </div>
+            <nav class="col-8">
+                <ul>
+                    <li><a href="<?php echo BASE_URL ?>">Главная</a> </li>
+                    
+                    <li><a href="encyclopedia.php">Энциклопедия</a> </li>
+
+                    <li>
+                        <?php if (isset($_SESSION['id'])): ?>
+                            <a href="#">
+                                <i class="fa fa-user"></i>
+                                <?php echo $_SESSION['login']; ?>
+                            </a>
+                            <ul>
+                                <?php if ($_SESSION['admin']): ?>
+                                    <li><a href="#">Админ панель</a> </li>
+                                <?php endif; ?>
+                                <li><a href="logout.php">Выход</a> </li>
+                            </ul>
+                        <?php else: ?>
+                            <a href="log.php"><i class="fa fa-user"></i>Войти</a>
+                            <ul>
+                                <li><a href="reg.php">Регистрация</a> </li>
+                            </ul>
+                        <?php endif; ?>
+
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
